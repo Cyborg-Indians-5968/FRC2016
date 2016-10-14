2adf988d20dad6658a4ddacea85986e6f9a86f9d
dash102 <stephaniefu@rocketmail.com>
2016-10-14 16:32:03 -0500

    Revert "Turn and Drive"
    This reverts commit 4dab106156cb0d161437caf8f5165bf150b71864.

--- robot/AutoShootManager.java
+++ robot/AutoShootManager.java
@@ -1,79 +1,89 @@
-package robot;
+package org.usfirst.frc.team5968.robot;
 
-import org.usfirst.frc.team5968.Robot.*;
 import org.usfirst.frc.team5968.robot.HumanInterface.BallFeedStates;
 
 public class AutoShootManager {
-	private final double angle_MAX = 0;
-	private final double angle_MIN = 0;
-	private final double distance_MIN = 20;
-	private final double distance_MAX = 27;
-	private double adjDist;
-	private Drive drive;
-	private BallShoot shoot;
-	private BallFeed feeder; //TODO: !!! This will always be null. Right now, the feeder is created by HumanInterface. It should be created in Robot instead and passed into both AutoShootManager and HumanInterface.
-	
+	private final double DISTANCE_TO_TRAVEL = 7;
+	private final double ERR = .01;
+
 	private int shootState = 0;
 	private int waitTime = 0;
-	
+
+	private Drive drive;
+	private BallShoot shoot;
+	private BallFeed feeder; // TODO: !!! This will always be null. Right now,
+								// the feeder is created by HumanInterface. It
+								// should be created in Robot instead and passed
+								// into both AutoShootManager and
+								// HumanInterface.
+
 	public AutoShootManager(Drive drive, BallShoot shoot) {
 		this.drive = drive;
 		this.shoot = shoot;
 	}
-	
-	public boolean ballShootComputer(double distance, double angle) 
-	{
-		double circumference = 7 * 2.54 * Math.PI * 10;
-		double distancePerPulse = (Math.PI * circumference)/360.0;
-		String angleDist = uART.aimToShoot();
-		int indexOfSpace = angleDist.indexOf(" ");
-		String angle = angleDist.substring(0,indexOfSpace);
-		double dist = Double.parseDouble(angleDist.substring(indexOfSpace + 1)); // In Millimeters
-		double angleDeg = (Double.parseDouble(angle) * 180) / Math.PI;
-		
-		if (angleDeg < 0) {
-			while (angleDeg != Drive.getGyro()) {
-				Drive.setRaw(-leftSpeed, rightSpeed);
+
+	public void driveBack() {
+		/*if (auto_state == AutoState.IDLE) {
+			dist = dir = 0;
+			auto_state = AutoState.DRIVE_BACKWARD;
+			dist_prev = drive.getDistance();
+		} else if (auto_state == AutoState.DONE) return;
+
+		dist += dir * (drive.getDistance() - dist_prev);
+		if (dist <= DISTANCE_TO_TRAVEL - ERR) {
+			dir = 1;
+			drive.driveStraight(true);
+		} else if (dist >= DISTANCE_TO_TRAVEL + ERR) {
+			dir = -1;
+			drive.driveStraight(false);
+		} else {
+			auto_state = AutoState.DONE;
+			drive.setRaw(0, 0);
+		}*/
+
+		// should this be non-blocking?
+		double dist_prev = drive.getDistance(), dist = 0, dir = 1;
+		while(true) {
+			dist += dir * (-dist_prev + (dist_prev = drive.getDistance()));
+			if (dist <= DISTANCE_TO_TRAVEL - ERR) {
+				dir = 1;
+				drive.driveStraight(true);
+			} else if (dist >= DISTANCE_TO_TRAVEL + ERR) {
+				dir = -1;
+				drive.driveStraight(false);
+			} else {
+				drive.setRaw(0, 0);
+				break;
 			}
-		} 
-		else if (angleDeg > 0) {
-			while (angleDeg != Drive.getGyro()) {
-				Drive.setRaw(leftSpeed, -rightSpeed);
+			// stall?
+		}
+	}
+
+	public boolean ballShootComputer(double distance, double angle) {
+
+		if (shootState == 0) {
+			shoot.turnOnShooter();
+			shootState = 1;
+		} else if (shootState == 1) {
+			waitTime += 20;
+
+			if (waitTime >= 260) {
+				shootState = 2;
+				waitTime = 0;
+			}
+		} else if (shootState == 2) {
+			feeder.ballFeed(BallFeedStates.FAST);
+			shootState = 3;
+		} else if (shootState == 3) {
+			waitTime += 20;
+
+			if (waitTime >= 1000) {
+				shoot.turnOffShooter();
+				feeder.ballFeed(BallFeedStates.STOPPED);
+				waitTime = 0;
+				return true;
 			}
 		}
-		
-		double leftSpeed = 0.8;
-        double rightSpeed = 0.8;
-        
-		leftEncoder.setDistancePerPulse(distancePerPulse);
-		rightEncoder.setDistancePerPulse(distancePerPulse);
-		double leftDist = Drive.leftEncoder.getDistance();
-		double rightDist = Drive.rightEncoder.getDistance();
-		
-		double distanceDiff = Math.abs(leftDistance - rightDistance);
-        
-        final double distanceThreshold = .1;
-        
-        if(distanceDiff > distanceThreshold)
-        {
-        	if(Math.abs(leftDist) > Math.abs(rightDist))
-        	{
-        		rightSpeed -= .1;
-        	}
-        	else
-        	{
-        		leftSpeed -= .1;
-        	}
-        }
-        if(!forward)
-        {
-        	leftSpeed *= -1.0;
-        	rightSpeed *= -1.0;
-        }
-		setRaw(leftSpeed, rightSpeed);
-		
-		
-		// uART.aimToShoot() returns "angle distance" (Radians millimeters)
-		
-	}//end of another method
+		return false;
+	}// end of another method
 }
